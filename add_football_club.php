d06b6e98a99e86d2ae851ee1c8308f9264195b3f
MahdiMirzaeiOfficial <MahdiMirzaeiOfficial@Gmail.com>
2021-03-01 10:17:40 +0330

    add_football_club

--- add_football_club.php
+++ add_football_club.php
@@ -5,8 +5,8 @@ include 'functions.php';
 $alert = '';
 
 if (isset($_POST['submit'])) {
-    $sql = "INSERT INTO football_club()
-VALUES('{$_POST['']}', '{$_POST['']}')";
+    $sql = "INSERT INTO fs_football_club(club_name, club_special_stadium_name, club_year_founding, club_motto, club_owner, club_founder, club_sponsors,     club_country, club_city, club_head_coach, club_logo, club_special_stadium_image, club_official_song)
+VALUES('{$_POST['club_name']}', '{$_POST['club_special_stadium_name']}', '{$_POST['club_year_founding']}', '{$_POST['club_motto']}', '{$_POST['club_owner']}', '{$_POST['club_founder']}', '{$_POST['club_sponsors']}', '{$_POST['club_country']}', '{$_POST['club_city']}', '{$_POST['club_head_coach']}', '{$_POST['club_logo']}', '{$_POST['club_special_stadium_image']}', '{$_POST['club_official_song']}')";
 
     $db = new DB();
 
@@ -28,7 +28,7 @@ VALUES('{$_POST['']}', '{$_POST['']}')";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>خدمات فوتبالی | افزودن باشگاه فوتبال</title>
+    <title>خدمات فوتبالی | ثبت باشگاه فوتبال</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.rtl.min.css" integrity="sha384-4dNpRvNX0c/TdYEbYup8qbjvjaMrgUPh+g4I03CnNtANuv+VAvPL6LqdwzZKV38G" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/main.css">
 </head>
@@ -37,7 +37,7 @@ VALUES('{$_POST['']}', '{$_POST['']}')";
     <section class="container-fluid">
         <header class="row">
             <section class="col-12">
-                <h1 class="text-center bg-primary text-light p-5">افزودن باشگاه فوتبال</h1>
+                <h1 class="text-center bg-primary text-light p-5">ثبت باشگاه فوتبال</h1>
             </section>
         </header>
         <main class="row justify-content-center mt-1">
